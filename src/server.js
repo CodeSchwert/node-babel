3b2c5a42ef247bfb7c0671bb4b40ca441f292423
Nik Ho <studying.mole@gmail.com>
2020-10-01 13:14:53 +1300

    Add marketplace POST and GET route, remove book GET route.

--- src/server.js
+++ src/server.js
@@ -1,23 +1,66 @@
 import express from 'express';
 import { connect } from './database';
-import Books from './models/bookModel';
+import Marketplaces from './models/marketplaceModel';
 
 connect();
 
+const PORT = 5000;
 const server = express();
 
-const PORT = 5000;
+server.use(express.json());
 
-server.get('/books', async (req, res) => {
+server.get('/api/marketplaces', async (req, res) => {
   try {
-    const books = await Books.find({});
-    console.log(books); // should be an array of objects
+    const marketplaces = await Marketplaces.find({});
+    console.log(marketplaces); // should be an array of objectsbooks
 
-    return res.json(books);
+    return res.json(marketplaces);
   } catch (e) {
     console.error(e);
     return res.status(500).send(e);
   }
+});
+
+server.post('/api/marketplaces', async (req, res) => {
+  try {
+    const { body } = req;
+    // console.log(body);
+
+    // check body properties - name, description, owner
+    if (
+      !body.hasOwnProperty('name') || 
+      !body.hasOwnProperty('description') ||
+      !body.hasOwnProperty('owner')
+    ) {
+      return res.status(400).json({ error: 'Marketplace name, description, owner required' });
+    }
+
+    // check if the marketplace already exists
+    const marketplaceExists = await Marketplaces.findOne({ name: body.name });
+    if (marketplaceExists != null) {
+      return res.status(400).json({ error: 'Marketplace name already in use' });
+    }
+
+    // use the model to create a new marketplace
+    const marketplace = new Marketplaces(body);
+    console.log(marketplace);
+
+    // save the marketplace
+    await marketplace.save();
+
+    // return 200 status and success message
+    return res.status(201).json({
+      success: true,
+      data: marketplace
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send(e);
+  }
+});
+
+server.use('*', (req, res) => {
+  return res.status(404).json({ error: 'Route not found' });
 });
 
 server.listen(PORT, () => {
